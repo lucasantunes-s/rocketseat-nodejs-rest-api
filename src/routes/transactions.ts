c465a0a2a811275aa39c353b7b36d1d2c7325b19
Lucas Antunes <lucasssantunes@hotmail.com>
2024-05-19 20:42:54 -0300

    Adding get all and get one routes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,30 @@
 import { FastifyInstance } from "fastify"
 import { knex } from "../database"
-import { z } from "zod"
+import { string, z } from "zod"
 import { randomUUID } from "crypto"
 
 
 export async function transactionsRoutes(app: FastifyInstance) {
+    app.get('/', async (request, reply) => {
+        const transactions = await knex('transactions').select()
+
+        return { transactions }
+    })
+    
+    app.get('/:id', async (request, reply) => {
+        console.log('id')
+
+        const getOneTransactionParamsSchema = z.object({
+            id: string().uuid()
+        })
+
+        const { id } = getOneTransactionParamsSchema.parse(request.params)
+
+        const transaction = await knex('transactions').where('id', id).first()
+
+        return { transaction }
+    })
+     
     app.post('/', async (request, reply) => {
         const createTransactionBodySchema = z.object({
             title: z.string(),
